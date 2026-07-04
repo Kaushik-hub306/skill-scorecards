e382b0cc8921d9052e34e79bdfe962e032a002d4
Kaushik-hub306 <ankamkaushik@gmail.com>
2026-07-03 22:20:53 -0500

    M0 scaffold: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export {
+  toGrade,
+  weightedScore,
+  overallGrade,
+  buildScorecard,
+  DEFAULT_WEIGHTS,
+  SAFETY_GATE,
+  METHODOLOGY_VERSION,
+} from "./score";
+export type { Grade, Dimension, DimensionScore, Scorecard } from "./score";
